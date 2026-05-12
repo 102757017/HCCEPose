f739d504be157743701b80c76570a293b515be4a
sunny <hewei@imasenwh.com>
2026-05-12 16:11:01 +0800

    使用 argparse 将所有可配置参数转换为命令行接口

--- yolo_train/train.py
+++ yolo_train/train.py
@@ -2,132 +2,73 @@
 # School of Mechanical Engineering, Southeast University, China
 
 '''
-The script `train.py` is used to train YOLOv11.  
-The original script was adapted from the OpenCV BPC project.  
-Project link: https://github.com/opencv/bpc  
-We added several augmentation strategies to enhance YOLO's performance.
+The script `s3_p1_prepare_yolo_label.py` is used to convert a BOP-format PBR training dataset into the YOLO format.  
+After specifying the path `xxx/xxx/demo-bin-picking` and running `s3_p1_prepare_yolo_label.py`,  
+a new folder named `yolo11` will be created under the `demo-bin-picking` directory.  
+The following is an example of the resulting folder structure:
+
+```
+demo-bin-picking
+|--- models
+|--- train_pbr
+|--- yolo11
+      |--- train_obj_s
+            |--- images
+            |--- labels
+            |--- yolo_configs
+                |--- data_objs.yaml
+            |--- autosplit_train.txt
+            |--- autosplit_val.txt
+```
 
 ------------------------------------------------------    
 
-脚本 `train.py` 用于训练 YOLOv11。  
-原始脚本改编自 OpenCV BPC 项目。  
-项目链接：https://github.com/opencv/bpc  
-在此基础上，增加了多种数据增强策略，以提升 YOLO 的性能。
+脚本 `s3_p1_prepare_yolo_label.py` 的功能是将 BOP 格式的 PBR 训练数据集转换为 YOLO 格式的数据集。  
+在指定 `xxx/xxx/demo-bin-picking` 路径后运行 `s3_p1_prepare_yolo_label.py`，  
+程序会在 `demo-bin-picking` 文件夹下生成一个名为 `yolo11` 的文件夹。  
+以下示例展示了生成后的文件夹结构：
+
+```
+demo-bin-picking
+|--- models
+|--- train_pbr
+|--- yolo11
+      |--- train_obj_s
+            |--- images
+            |--- labels
+            |--- yolo_configs
+                |--- data_objs.yaml
+            |--- autosplit_train.txt
+            |--- autosplit_val.txt
+```
 '''
 
-import os
-import sys
-import argparse
-from ultralytics import YOLO
+import os, json
+from yolo_train.label import convert_train_pbr_2_yolo, generate_yaml
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(current_dir)
-print(current_dir)
-
-def train_yolo11(task, data_path, gpu_num, epochs, imgsz, batch):
-    """
-    Train YOLO11 for a specific task ("detection" or "segmentation")
-    using Ultralytics YOLO with a single object class.
-
-    Args:
-        task (str): "detection" or "segmentation"
-        data_path (str): Path to the YOLO .yaml file (e.g. data_obj_11.yaml).
-        obj_id (int): The BOP object ID (e.g. 11).
-        epochs (int): Number of training epochs.
-        imgsz (int): Image size used for training.
-        batch (int): Batch size.
-
-    Returns:
-        final_model_path (str): Path where the trained model is saved.
-    """
-
-    device = []
-    for i_ in range(int(gpu_num)):
-        device.append(i_)
+if __name__ == '__main__':
     
-    if task == "detection":
-        pretrained_weights = "yolo11x.pt"
-        task_suffix = "detection"
-    elif task == "segmentation":
-        pretrained_weights = "yolo11n-seg.pt"
-        task_suffix = "segmentation"
-    else:
-        print("Invalid task. Must be 'detection' or 'segmentation'.")
-        return None
-    if not os.path.exists(data_path):
-        print(f"Error: Dataset YAML file not found at {data_path}")
-        return None
-    print(f"Loading model {pretrained_weights} for {task_suffix} ...")
+    # Specify the path to the dataset folder.
+    # 指定数据集文件夹的路径。
+    dataset_path = '/kaggle/working/HCCEPose/demo-bin-picking'
     
-    last_pt = os.path.join(os.path.dirname(os.path.dirname(data_path)), 'train', 'weights', 'last.pt')
-    if os.path.exists(last_pt):
-        model = YOLO(last_pt)
-        resume = True
-    else:
-        model = YOLO(pretrained_weights)
-        resume = False
-    model.train(
-        data=data_path,
-        epochs=epochs,
-        imgsz=imgsz,
-        batch=batch,
-        device=device,
-        val = True, 
-        fraction = 1.00,
-        workers=8,  
-        save=True,  
-        save_period = 1,
-        project = os.path.dirname(os.path.dirname(data_path)),
-        close_mosaic = 10,
-        label_smoothing=0.0,  
-        degrees=0.0,       
-        translate=0.1,      
-        scale=0.50,     
-        shear=0.0,       
-        perspective=0.0,    
-        flipud=0.5,      
-        fliplr=0.5,       
-        mosaic=1.0,     
-        mixup=1.0,         
-        copy_paste = 1.0,
-        copy_paste_mode = 'mixup',
-        resume=resume,
-        dropout = 0.2,
-        auto_augment = 'AugMix',
-        freeze=0, 
-        multi_scale=True,
-    )
-    save_dir = os.path.join(os.path.dirname(os.path.dirname(data_path)), task_suffix, f"obj_s")
-    os.makedirs(save_dir, exist_ok=True)
-    model_name = f"yolo11-{task_suffix}-obj_s.pt"
-    final_model_path = os.path.join(os.path.dirname(os.path.dirname(data_path)), save_dir, model_name)
-    model.save(final_model_path)
-    print(f"Model saved as: {final_model_path}")
-    return final_model_path
-
-def main():
-    parser = argparse.ArgumentParser(description="Train YOLO11 on a specific dataset and object.")
-    parser.add_argument("--data_path", type=str, required=True, 
-                        help="Path to the dataset YAML file (e.g. idp_codebase/yolo/configs/data_obj_11.yaml).")
-    parser.add_argument("--epochs", type=int, default=300, help="Number of training epochs.")
-    parser.add_argument("--imgsz", type=int, default=640, help="Input image size.")
-    parser.add_argument("--batch", type=int, default=16, help="Batch size for training.")
-    parser.add_argument("--gpu_num", type=int, default=1, help="Number of GPUs.")
-    parser.add_argument("--task", type=str, choices=["detection", "segmentation"], default="detection",
-                        help="Task type (detection or segmentation).")
-
-    args = parser.parse_args()
-
-    train_yolo11(
-        task=args.task,
-        data_path=args.data_path,
-        gpu_num = args.gpu_num,
-        # obj_id=args.obj_id,
-        epochs=args.epochs,
-        imgsz=args.imgsz,
-        batch=args.batch
-    )
-
-
-if __name__ == "__main__":
-    main()
+    # Extract the dataset name, the path to the `train_pbr` folder, and the output path for YOLO labels based on the given dataset path.
+    # 根据给定的数据集路径，提取数据集名称、`train_pbr` 文件夹路径以及 YOLO 标签的输出路径。
+    dataset_name = os.path.join(dataset_path)
+    dataset_path = os.path.join(dataset_path, '%s/train_pbr'%dataset_name)
+    output_path  = os.path.join(dataset_path, '%s/yolo11/train_obj_s'%dataset_name)
+    
+    # Extract all object IDs from the models_info.json file.
+    # 根据 models_info.json 文件提取所有物体 ID。
+    obj_id_list = []
+    with open(os.path.join(dataset_path, '%s/models/models_info.json'%dataset_name), "r") as f:
+        scene_gt_data = json.load(f)
+    for key_ in scene_gt_data:
+        obj_id_list.append(key_)
+        
+    # Convert the BOP-format `train_pbr` dataset into a YOLO-format dataset.
+    # 将 BOP 格式的 `train_pbr` 数据集转换为 YOLO 格式的数据集。
+    convert_train_pbr_2_yolo(dataset_path, output_path, obj_id_list)
+    generate_yaml(output_path, obj_id_list)
+    print("[INFO] Dataset preparation complete!")
+    pass

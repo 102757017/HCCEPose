57d3106821197cca96cd561762e5bafae6d6959b
sunny <hewei@imasenwh.com>
2026-05-13 17:40:25 +0800

    new

---
+++ flow.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#渲染数据集
+chmod +x s2_p1_gen_pbr_data.sh
+./s2_p1_gen_pbr_data.sh \
+    --gpu_id 0 \
+    --scene_num 2 \
+    --cc0textures /kaggle/working/HCCEPose/cc0textures-512 \
+    --dataset_path ./demo-bin-picking \
+    --script_path /kaggle/working/HCCEPose/s2_p1_gen_pbr_data.py | grep -v "Rendering frame"
+
+#cd demo-bin-picking
+#python ..\s2_p1_gen_pbr_data.py --gpu_id 0 --cc0textures "E:/python/HCCEPose/cc0textures-512" --scene_num 2
+
+#生成yolo数据集
+python s3_p1_prepare_yolo_label.py --dataset_path ./demo-bin-picking
+
+
+#训练yolo模型
+python s3_p2_train_yolo.py --dataset_path ./demo-bin-picking --gpu_num 2 --batch_size 8 --epochs 1
+
+#物体正背面标签制备
+python s4_p1_gen_bf_labels.py --dataset_path ./demo-bin-picking
+
+
+#训练 HccePose
+python -m torch.distributed.launch --nproc_per_node=2 s4_p2_train_bf_pbr_ddp.py --dataset_path ./demo-bin-picking

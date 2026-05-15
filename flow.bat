646aff68fd1a095ccfd23636a22a225ec632a13a
sunny <hewei@imasenwh.com>
2026-05-15 15:51:34 +0800

    window材质路径要使用绝对路径

--- flow.bat
+++ flow.bat
@@ -7,7 +7,7 @@ call .venv\Scripts\activate.bat
 REM 渲染数据集（原 s2_p1_gen_pbr_data.sh 逻辑）
 set GPU_ID=0
 set SCENE_NUM=2
-set CC0TEXTURES=..\cc0textures-512
+set CC0TEXTURES=E:\python\HCCEPose\cc0textures-512
 set DATASET_PATH=.\demo-bin-picking
 set SCRIPT_PATH=..\s2_p1_gen_pbr_data.py
 
@@ -23,12 +23,12 @@ REM 生成 YOLO 数据集
 python s3_p1_prepare_yolo_label.py --dataset_path %DATASET_PATH%
 
 REM 训练 YOLO 模型
-python s3_p2_train_yolo.py --dataset_path %DATASET_PATH% --gpu_num 2 --batch_size 8 --epochs 1
+python s3_p2_train_yolo.py --dataset_path %DATASET_PATH% --gpu_num 1 --batch_size 8 --epochs 1
 
-REM 物体正背面标签制备
-python s4_p1_gen_bf_labels.py --dataset_path %DATASET_PATH%
+REM 物体正背面标签制备，windows下需要关闭虚拟显示器
+python s4_p1_gen_bf_labels.py --dataset_path %DATASET_PATH%   --no_display
 
 REM 训练 HccePose（分布式）
-python -m torch.distributed.launch --nproc_per_node=2 s4_p2_train_bf_pbr_ddp.py --dataset_path %DATASET_PATH% --start_obj_id 1 --end_obj_id 1 --total_iteration 10
+python -m torch.distributed.launch --nproc_per_node=1 s4_p2_train_bf_pbr_ddp.py --dataset_path %DATASET_PATH% --start_obj_id 1 --end_obj_id 1 --total_iteration 10
 
 pause
